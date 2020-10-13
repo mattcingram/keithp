39186ec48ac33e61cff7f5c865d5c3d612c63042
Matt Ingram <40709229+mattcingram@users.noreply.github.com>
2020-10-13 12:02:45 -0400

    Create install.R

---
+++ install.R
@@ -0,0 +1,6 @@
+  
+# packages from original publication
+mypackages <- c("ggplot2", "tidyverse", "mcmcplots", "lme4", "arm", "lattice", "rpart", "broom", "broom.mixed", "modelsummary", "beepr")
+install.packages(mypackages, verbose=TRUE)
+# additional packages
+install.packages(c("pacman", "stargazer", "data.table", "xtable"))
